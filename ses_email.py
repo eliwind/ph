636f71a83a6c9b1ed01f926e630652adcf7f5526
Eli Daniel <eli.daniel@gmail.com>
2014-10-02 14:15:34 +0000

    use parenthelp@ account for sending email instead of eli

--- ses_email.py
+++ ses_email.py
@@ -4,14 +4,14 @@ from email.mime.text import MIMEText
 def sendmail (toaddr, subject, body):
     msg = MIMEText(body)
     msg['Subject'] = subject
-    msg['From'] = 'eli.daniel@gmail.com'
+    msg['From'] = 'parenthelp@agassizpreschool.org'
     msg['To'] = toaddr
     
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
     s = smtplib.SMTP_SSL('email-smtp.us-east-1.amazonaws.com')
     s.login ('AKIAJCX6NGII4NPM2ONA', 'Aho1fIvhLaL368fjxCGfnlD4pIkKLwsyfscxzBRU9FDk' )
-    s.sendmail('eli.daniel@gmail.com', toaddr, msg.as_string())
+    s.sendmail('parenthelp@agassizpreschool.org', toaddr, msg.as_string())
     s.quit()
 
 
