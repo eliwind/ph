bd5b5adeba092236bfcb00ed66eeac97f41d224f
Eli Daniel <eli.daniel@gmail.com>
2015-09-11 01:36:27 +0000

    Change to right working directory for background email tasks (so we pick up config)

--- announce.sh
+++ announce.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd /ph
 /ph/.virtualenvs/ph/bin/python /ph/announce.py > /ph/announce.out 2>&1
  
--- reminder.sh
+++ reminder.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd /ph
 /ph/.virtualenvs/ph/bin/python /ph/reminder.py > /ph/reminder.out 2>&1
  
